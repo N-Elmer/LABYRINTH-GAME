388e9c7f31939d1c4eb85d2be1edca724ae79ddb
N-Elmer <elmerelmander@gmail.com>
2023-04-04 19:11:20 +0100

    LABYRINTH-GAME
    3 DIMENSION ESCAPE 🎮🕹 PATH FINDING GAME

---
+++ Assets/scripts/link.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Link : MonoBehaviour
+{
+
+    protected Joystick Joystick;
+    protected joybuttonscript joybuttonscript;
+    // Use this for initialization
+    void Start()
+    {
+        Joystick = FindObjectOfType<Joystick>();
+        joybuttonscript = FindObjectOfType<joybuttonscript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var rigidbody = GetComponent<Rigidbody>();
+        rigidbody.velocity = new Vector3(Joystick.Horizontal * 10f,
+                                            rigidbody.velocity.y,
+                                            Joystick.Vertical * 10f);
+
+
+    }
+}
---
+++ Assets/scripts/playercontroller.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playercontroller : MonoBehaviour {
+    public float speed;
+    public Text countText;
+    public Text wintext;
+
+    private Rigidbody rb;
+    private int COUNT;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        COUNT = 0;
+        setCountText();
+        wintext.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            COUNT = COUNT + 1;
+            setCountText();
+        }
+    }
+
+    void setCountText ()
+    {
+        countText.text = "CUBES " + COUNT.ToString();
+        if (COUNT >= 42)
+        {
+            wintext.text = "YOU WIN!";
+        }
+        else
+        {
+            wintext.text = "COLLECT ALL CUBES";
+        }
+    }
+}
